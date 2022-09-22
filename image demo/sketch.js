99be8c7dc54e4a470477fccbedee2ccda9fe701e
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-09-22 11:55:47 -0600

    update image learning demo

---
+++ image demo/sketch.js
@@ -0,0 +1,21 @@
+// Image Demo
+// Doan Khanh Vinh
+// 11:37 AM 9/22/2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let fishImage;
+let scalar = 0.5;
+function preload() {
+  fishImage = loadImage("Fish.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  image(fishImage,width/2,height/2,fishImage.width*scalar, fishImage.height*scalar);
+}
