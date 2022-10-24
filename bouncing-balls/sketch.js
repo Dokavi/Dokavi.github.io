2d4915196901c227bd5fdd3ab5dac75c5a89a72d
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-10-24 11:35:54 -0600

    bouncing balls

---
+++ bouncing-balls/sketch.js
@@ -0,0 +1,24 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let a = {
+  x: 10,
+  y: 10,
+  d: 50
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function displayBalls() {
+  circle(a.x,a.y,a.d);
+}
