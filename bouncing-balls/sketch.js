a3b663d00a575e2da82ead4c963c72e87606cb6f
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-10-24 12:17:18 -0600

    bouncing balls

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -16,16 +16,17 @@ function draw() {
   displayCircles();
   moveCircles();
   bounceWall();
+  bounceBall();
 }
 
 function mousePressed() {
   spawnCircle();
 }
 function displayCircles() {
-  for (let i = 0; i < theCircle.length; i++) {
+  for (let thisCirle of theCircle) {
     noStroke();
-    fill(theCircle[i].theColor);
-    circle(theCircle[i].x,theCircle[i].y,theCircle[i].radius*2);
+    fill(thisCirle.theColor);
+    circle(thisCirle.x,thisCirle.y,thisCirle.radius*2);
   }
 }
 
@@ -33,10 +34,10 @@ function spawnCircle() {
   let thisCircle ={
     x: mouseX,
     y: mouseY,
-    radius: 30,
-    theColor: color(random(255),random(255),random(255)),
-    xVelocity: 2,
-    yVelocity: 1.5,
+    radius: 50,
+    theColor: color(random(255),random(255),random(255),random(255)),
+    xVelocity: random(-5,5),
+    yVelocity: random(-5,5)
   };
   theCircle.push(thisCircle);
 }
@@ -50,17 +51,25 @@ function moveCircles() {
 
 function bounceWall() {
   for (let i = 0; i < theCircle.length; i++) {
-    if (theCircle[i].x >=width) {
+    if (theCircle[i].x +theCircle[i].radius >=width || theCircle[i].x - theCircle[i].radius<=0) {
       theCircle[i].xVelocity *=-1;
     }
-    if (theCircle[i].x <=0) {
-      theCircle[i].xVelocity *=-1;
-    }
-    if (theCircle[i].y >=height) {
+    if (theCircle[i].y +theCircle[i].radius >=height || theCircle[i].y -theCircle[i].radius <=0) {
       theCircle[i].yVelocity *=-1;
     }
-    if (theCircle[i].y <=0) {
-      theCircle[i].yVelocity *=-1;
+  }
+}
+
+function bounceBall() {
+  let collisionArray = [...theCircle];
+  for (let i = 0; i < theCircle.length; i++) {
+    let a = collisionArray.shift(collisionArray[i]);
+    for (let cirlceCheck of collisionArray) {
+      let hit = collideCircleCircle(a.x,a.y,a.radius*2,cirlceCheck.x,cirlceCheck.y,cirlceCheck.radius*2);
+      if (hit) {
+        a.xVelocity *=-1;a.yVelocity *=-1;
+      }
+      collisionArray.unshift(collisionArray[i]);
     }
   }
 }
