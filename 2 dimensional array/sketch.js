b9da6b2bc46a8ee5e4d479546eff80d883b98893
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-10-25 12:17:02 -0600

    update

--- 2 dimensional array/sketch.js
+++ 2 dimensional array/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = [[0,0,1],
+let grid = [[0,1,1],
             [1,0,1],
             [0,1,0]];
 
@@ -31,5 +31,18 @@ function displayGrid(grid) {
       }
       rect(x*cellWidth,y*cellHeight,cellWidth,cellHeight);
     }
+  }
+}
+
+function mousePressed() {
+  let cellWidth = width/grid[0].length;
+  let cellHeight = height/grid.length;
+  let x = Math.floor(mouseX/cellWidth);
+  let y = Math.floor(mouseY/cellHeight);
+  if (grid[y][x] === 0) {
+    grid[y][x] = 1;
+  }
+  else if (grid[x][y] === 1) {
+    grid[y][x] = 0;
   }
 }
--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -5,90 +5,150 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let theCircle = [];
-let hit = false;
+// let theCircle = [];
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+
+// function draw() {
+//   background(220);
+//   displayCircles();
+//   move();
+// }
+
+// function mousePressed() {
+//   spawnCircle();
+// }
+// function displayCircles() {
+//   for (let thisCirle of theCircle) {
+//     noStroke();
+//     fill(thisCirle.theColor);
+//     circle(thisCirle.x,thisCirle.y,thisCirle.radius*2);
+//   }
+// }
+
+// function spawnCircle() {
+//   let thisCircle ={
+//     x: mouseX,
+//     y: mouseY,
+//     radius: 50,
+//     theColor: color(random(255),random(255),random(255),random(255)),
+//     xVelocity: random(-5,5),
+//     yVelocity: random(-5,5)
+//   };
+//   theCircle.push(thisCircle);
+// }
+
+
+// function move() {
+//   for (let i=0; i<theCircle.length; i++) {
+//     theCircle[i].x += theCircle[i].dx;
+//     theCircle[i].y += theCircle[i].dy;
+
+//     //collision check
+//     for (let j=0; j<theCircle.length; j++) {
+//       if (i !== j) { //don't check if hitting self
+//         if (isColliding(theCircle[i], theCircle[j])) {
+//           //swap the speeds
+//           let tempDx = theCircle[i].dx;
+//           let tempDy = theCircle[i].dy;
+//           theCircle[i].dx = theCircle[j].dx;
+//           theCircle[i].dy = theCircle[j].dy;
+//           theCircle[j].dx = tempDx;
+//           theCircle[j].dy = tempDy;
+//         }
+//       }
+//     }
+
+//     //left-right edges
+//     if (theCircle[i].x + theCircle[i].radius > width ||
+//        theCircle[i].x - theCircle[i].radius < 0) {
+//       theCircle[i].dx *= -1;
+//     }
+
+//     //top-bottom edges
+//     if (theCircle[i].y + theCircle[i].radius > height || 
+//       theCircle[i].y - theCircle[i].radius < 0) {
+//       theCircle[i].dy *= -1;
+//     }
+//   }
+// }
+
+// // function moveBall() {
+// //   for (let i = 0; i < theCircle.length; i++) {
+// //     let a = theCircle.shift(theCircle[i]);
+// //     for (let cirlceCheck of theCircle) {
+// //       hit = collideCircleCircle(theCircle[i].x,theCircle[i].y,theCircle[i].radius*2,cirlceCheck.x,cirlceCheck.y,cirlceCheck.radius*2);
+// //       if (hit) {
+// //         a.xVelocity *=-1;a.yVelocity *=-1;
+// //       }
+// //       theCircle.unshift(theCircle[i]);
+// //       hit = false;
+// //     }
+// //   }
+// // }
+
+// function isColliding(ball1, ball2) {
+//   let distanceBetween = dist(ball1.x, ball1.y, ball2.x, ball2.y);
+//   let radiiSum = ball1.radius + ball2.radius;
+//   if (distanceBetween > radiiSum) {
+//     return false;
+//   }
+//   else {
+//     return true;
+//   }
+// }
+let theCircles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theCircles.push(spawnBall(100, 100));
 }
 
 function draw() {
   background(220);
-  displayCircles();
-  moveCircles();
-  bounce();
-}
 
-function mousePressed() {
-  spawnCircle();
-}
-function displayCircles() {
-  for (let thisCirle of theCircle) {
-    noStroke();
-    fill(thisCirle.theColor);
-    circle(thisCirle.x,thisCirle.y,thisCirle.radius*2);
-  }
-}
+  //move
+  for (let i=0; i<theCircles.length; i++) {
+    theCircles[i].x += theCircles[i].dx;
+    theCircles[i].y += theCircles[i].dy;
 
-function spawnCircle() {
-  let thisCircle ={
-    x: mouseX,
-    y: mouseY,
-    radius: 50,
-    theColor: color(random(255),random(255),random(255),random(255)),
-    xVelocity: random(-5,5),
-    yVelocity: random(-5,5)
-  };
-  theCircle.push(thisCircle);
-}
-
-function moveCircles() {
-  for (let i = 0; i < theCircle.length; i++) {
-    theCircle[i].x += theCircle[i].xVelocity;
-    theCircle[i].y += theCircle[i].yVelocity;
-  }
-}
-
-function bounce() {
-  //edges
-  for (let i = 0; i < theCircle.length; i++) {
-    for (let j=0; j<theCircle.length; j++) {
+    //collision check
+    for (let j=0; j<theCircles.length; j++) {
       if (i !== j) { //don't check if hitting self
-        if (isColliding(theCircle[i], theCircle[j])) {
+        if (isColliding(theCircles[i], theCircles[j])) {
           //swap the speeds
-          let tempDx = theCircle[i].dx;
-          let tempDy = theCircle[i].dy;
-          theCircle[i].dx = theCircle[j].dx;
-          theCircle[i].dy = theCircle[j].dy;
-          theCircle[j].dx = tempDx;
-          theCircle[j].dy = tempDy;
+          let tempDx = theCircles[i].dx;
+          let tempDy = theCircles[i].dy;
+          theCircles[i].dx = theCircles[j].dx;
+          theCircles[i].dy = theCircles[j].dy;
+          theCircles[j].dx = tempDx;
+          theCircles[j].dy = tempDy;
         }
       }
     }
-    if (theCircle[i].x +theCircle[i].radius >=width || theCircle[i].x - theCircle[i].radius<=0) {
-      theCircle[i].xVelocity *=-1;
-    }
-    if (theCircle[i].y +theCircle[i].radius >=height || theCircle[i].y -theCircle[i].radius <=0) {
-      theCircle[i].yVelocity *=-1;
-    }
-    //collision check
 
+    //left-right edges
+    if (theCircles[i].x + theCircles[i].radius > width ||
+       theCircles[i].x - theCircles[i].radius < 0) {
+      theCircles[i].dx *= -1;
+    }
+
+    //top-bottom edges
+    if (theCircles[i].y + theCircles[i].radius > height || 
+      theCircles[i].y - theCircles[i].radius < 0) {
+      theCircles[i].dy *= -1;
+    }
+  }
+  
+  //display
+  for (let thisCircle of theCircles) {
+    fill(thisCircle.theColor);
+    noStroke();
+    circle(thisCircle.x, thisCircle.y, thisCircle.radius*2);
   }
 }
-
-// function bounceBall() {
-//   for (let i = 0; i < theCircle.length; i++) {
-//     let a = theCircle.shift(theCircle[i]);
-//     for (let cirlceCheck of theCircle) {
-//       hit = collideCircleCircle(theCircle[i].x,theCircle[i].y,theCircle[i].radius*2,cirlceCheck.x,cirlceCheck.y,cirlceCheck.radius*2);
-//       if (hit) {
-//         a.xVelocity *=-1;a.yVelocity *=-1;
-//       }
-//       theCircle.unshift(theCircle[i]);
-//       hit = false;
-//     }
-//   }
-// }
 
 function isColliding(ball1, ball2) {
   let distanceBetween = dist(ball1.x, ball1.y, ball2.x, ball2.y);
@@ -99,4 +159,20 @@ function isColliding(ball1, ball2) {
   else {
     return true;
   }
+}
+
+function mousePressed() {
+  theCircles.push(spawnBall(mouseX, mouseY));
+}
+
+function spawnBall(tempX, tempY) {
+  let newBall = {
+    x: tempX, 
+    y: tempY,
+    radius: random(25, 100),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    theColor: color(random(255), random(255), random(255), random(255))
+  };
+  return newBall;
 }
