328858506bc86ff2deefdf68455a14b2c97b30dc
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-10-25 11:36:43 -0600

    update bouncing ball

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let theCircle = [];
+let hit = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,8 +16,7 @@ function draw() {
   background(220);
   displayCircles();
   moveCircles();
-  bounceWall();
-  bounceBall();
+  bounce();
 }
 
 function mousePressed() {
@@ -49,7 +49,8 @@ function moveCircles() {
   }
 }
 
-function bounceWall() {
+function bounce() {
+  //edges
   for (let i = 0; i < theCircle.length; i++) {
     if (theCircle[i].x +theCircle[i].radius >=width || theCircle[i].x - theCircle[i].radius<=0) {
       theCircle[i].xVelocity *=-1;
@@ -57,19 +58,44 @@ function bounceWall() {
     if (theCircle[i].y +theCircle[i].radius >=height || theCircle[i].y -theCircle[i].radius <=0) {
       theCircle[i].yVelocity *=-1;
     }
+    //collision check
+    for (let j; j<theCircle.length;j++) {
+      if (i !== j) { //don't hit itself
+        if (isColliding(theCircle[i],theCircle[j])) {
+          let tempDx = theCircle[i].xVelocity;
+          let tempDy = theCircle[i].yVelocity;
+          theCircle[i].xVelocity = theCircle[j].xVelocity;
+          theCircle[i].yVelocity = theCircle[j].yVelocity;
+          theCircle[j].xVelocity = tempDx;
+          theCircle[j].yVelocity = tempDy;
+        }
+      }
+    }
   }
 }
 
-function bounceBall() {
-  let collisionArray = [...theCircle];
-  for (let i = 0; i < theCircle.length; i++) {
-    let a = collisionArray.shift(collisionArray[i]);
-    for (let cirlceCheck of collisionArray) {
-      let hit = collideCircleCircle(a.x,a.y,a.radius*2,cirlceCheck.x,cirlceCheck.y,cirlceCheck.radius*2);
-      if (hit) {
-        a.xVelocity *=-1;a.yVelocity *=-1;
-      }
-      collisionArray.unshift(collisionArray[i]);
-    }
+// function bounceBall() {
+//   for (let i = 0; i < theCircle.length; i++) {
+//     let a = theCircle.shift(theCircle[i]);
+//     for (let cirlceCheck of theCircle) {
+//       hit = collideCircleCircle(theCircle[i].x,theCircle[i].y,theCircle[i].radius*2,cirlceCheck.x,cirlceCheck.y,cirlceCheck.radius*2);
+//       if (hit) {
+//         a.xVelocity *=-1;a.yVelocity *=-1;
+//       }
+//       theCircle.unshift(theCircle[i]);
+//       hit = false;
+//     }
+//   }
+// }
+
+function isColliding(ball1,ball2) {
+  let d = dist(ball1.x,ball1.y,ball2.x,ball2.y);
+  let radiiSum = ball1.radius + ball2.radius;
+  // return !(d>radiiSum);
+  if (d > radiiSum) {
+    return false;
+  }
+  else {
+    return true;
   }
 }
