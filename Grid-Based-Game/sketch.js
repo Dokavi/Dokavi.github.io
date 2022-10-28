9c52e0877d2a6cb4e602740d1dc5f89788515afe
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-10-28 12:22:42 -0600

    assignment 2

---
+++ Grid-Based-Game/sketch.js
@@ -0,0 +1,82 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+const ROWS = 40;
+const COLS = 40;
+let grid;
+let cellWidth;
+let cellHeight;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cellHeight = height/ROWS;
+  cellWidth = width/COLS;
+  grid = createRandom2dArray(ROWS,COLS);
+}
+
+function draw() {
+  background(220);
+  displayGrid(grid);
+}
+
+function create2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y = 0;y<ROWS;y++) {
+    emptyArray.push([]);
+    for (let x = 0;x<COLS;x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function displayGrid(grid) {
+  for (let y = 0;y<ROWS;y++) {
+    for (let x = 0;x<COLS;x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+        rect(x*cellWidth,y*cellHeight,cellWidth,cellHeight);
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+        rect(x*cellWidth,y*cellHeight,cellWidth,cellHeight);
+      }
+    }
+  }
+}
+
+function createRandom2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y = 0;y<ROWS;y++) {
+    emptyArray.push([]);
+    for (let x = 0;x<COLS;x++) {
+      if (random(100) <50) {
+        emptyArray[y].push(0);
+      }
+      else{
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellWidth);
+  let y = Math.floor(mouseY/cellHeight);
+  if (grid[y][x] === 0) {
+    grid[y][x] =1;
+  }
+  else if (grid[y][x] === 1) {
+    grid[y][x] =0;
+  }
+}
+
+function mouseDragged() {
+  
+}
