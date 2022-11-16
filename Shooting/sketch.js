5075b3777d2e753ada3bac7cf78c976c15f317e3
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-11-16 12:09:05 -0600

    shooting

---
+++ Shooting/sketch.js
@@ -0,0 +1,51 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+class Bullet {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.size =5;
+    this.dx = 5;
+  }
+  move() {
+    this.x +=this.dx;
+  }
+  display() {
+    circle(this.x,this.y,this.size);
+  }
+  isDead() {
+    return this.x <=0 || this.x >= width;
+  }
+}
+let person = {
+  x:100,
+  y:0
+};
+let bulletsCase = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  person.y = height -100;
+}
+
+function draw() {
+  background(220);
+  rect(person.x,person.y,50,50);
+  for (let i = 0; i<bulletsCase.length;i++) {
+    bulletsCase[i].move();
+    bulletsCase[i].display();
+    if (bulletsCase[i].isDead()) {
+      bulletsCase.Splice(i,1);
+    }
+  }
+}
+
+function mousePressed() {
+  let bulletShot = new Bullet(person.x,person.y);
+  bulletsCase.push(bulletShot);
+}
